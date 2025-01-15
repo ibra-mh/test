f74a0f3d2df0f4d88f37281fa21111f766cac99d
ibrahimhand <ibramhand555@gmail.com>
2025-01-15 21:26:02 +0100

    utils

--- utils/application_properties.go
+++ utils/application_properties.go
@@ -1,1 +1,24 @@
 package utils
+
+import (
+        "encoding/json"
+        "log"
+        "os"
+)
+
+func LoadProperties(path string, target interface{}) error {
+        file, err := os.Open(path)
+        if err != nil {
+                log.Fatal("Error opening properties file:", err)
+                return err
+        }
+        defer file.Close()
+
+        decoder := json.NewDecoder(file)
+        err = decoder.Decode(target)
+        if err != nil {
+                log.Fatal("Error decoding properties file:", err)
+                return err
+        }
+        return nil
+}
