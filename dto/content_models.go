c2395cfad37dede540126e8aa2bafc4f87fbd52a
ibrahimhand <ibramhand555@gmail.com>
2025-01-15 21:25:47 +0100

    dto

--- dto/content_models.go
+++ dto/content_models.go
@@ -1,3 +1,33 @@
 package dto
 
+import "time"
+
+type Path struct {
+        ID          int       `json:"id"`
+        Name        string    `json:"name"`
+        GUID        string    `json:"guid"`
+        Description string    `json:"description"`
+        CreatedAt   time.Time `json:"created_at"`
+        UpdatedAt   time.Time `json:"updated_at"`
+        DeletedAt   *time.Time `json:"deleted_at"` // Use pointer for nullable
+}
+
+type Content struct {
+        ID          int       `json:"id"`
+        Name        string    `json:"name"`
+        GUID        string    `json:"guid"`
+        Description string    `json:"description"`
+        CreatedAt   time.Time `json:"created_at"`
+        UpdatedAt   time.Time `json:"updated_at"`
+        DeletedAt   *time.Time `json:"deleted_at"`
+}
+
+type ContentPath struct {
+        ID        int       `json:"id"`
+        ContentID int       `json:"content_id"`
+        PathID    int       `json:"path_id"`
+        CreatedAt time.Time `json:"created_at"`
+        UpdatedAt time.Time `json:"updated_at"`
+        DeletedAt *time.Time `json:"deleted_at"`
+}
 
