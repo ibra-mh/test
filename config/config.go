16a9141992605b98d7f726ae2b2ae16cda7c52bc
ibrahimhand <ibramhand555@gmail.com>
2025-01-15 20:36:49 +0100

    idk

---
+++ config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+        "encoding/json"
+        "os"
+)
+
+type Config struct {
+        Database struct {
+                Host     string `json:"host"`
+                Port     string `json:"port"`
+                User     string `json:"user"`
+                Password string `json:"password"`
+                Name     string `json:"dbname"`
+                SSLMode  string `json:"sslmode"` // e.g., "disable" or "require"
+        } `json:"database"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+        file, err := os.Open(path)
+        if err != nil {
+                return nil, err
+        }
+        defer file.Close()
+
+        var config Config
+        decoder := json.NewDecoder(file)
+        err = decoder.Decode(&config)
+        if err != nil {
+                return nil, err
+        }
+
+        return &config, nil
+}
